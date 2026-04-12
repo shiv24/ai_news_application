e8914e41e4e601bba5d6fded2131a2fb0d0ac215
Shiv Gupta <shivg56@gmail.com>
2026-04-12 15:03:01 -0400

    adding the frontend render url to allow_origins

--- backend/src/main.py
+++ backend/src/main.py
@@ -12,6 +12,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8080",
         "http://127.0.0.1:8080",
+        "https://ai-news-application-ahiv.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
